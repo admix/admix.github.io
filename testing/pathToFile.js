57e88374e05f238ada562ad2e842108a3523d2e7
admix <admix.snurnikov@gmail.com>
2014-02-17 21:21:29 -0500

    Added example with data-* attribute

---
+++ testing/pathToFile.js
@@ -0,0 +1,4 @@
+var first = document.getElementById("your-id").getAttribute("data-anyVariable");
+var second = document.getElementById("your-id").getAttribute("data-anyOtherVariable");
+
+alert(first + "\n" + second);
