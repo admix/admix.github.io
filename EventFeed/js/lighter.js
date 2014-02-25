22d12a946cc24a7af892d3103a1eec99c6537c2a
admix <admix.snurnikov@gmail.com>
2014-02-25 16:41:49 -0500

    New design of eventfeed landing page

---
+++ EventFeed/js/lighter.js
@@ -0,0 +1,37 @@
+function SwitchOnOff(){
+	var inL = getCssProperty("switcher", "background");
+	var outL = getCssProperty("body_d","background");
+	var lightOn = "#dddd44";
+	var lightOff = "#000";
+	//alert(getCssProperty("switcher", "background-color"));
+	if(inL == outL){
+		document.getElementById("body_d").style.background=lightOn;
+		document.getElementById("switcher").style.background=lightOff;
+
+		//var light = "<style>.light{background-color: #000;}</style>";
+	}else{
+		document.getElementById("body_d").style.background=lightOn;
+		document.getElementById("switcher").style.background=lightOn;
+		//var light = "<style>.light{background-color: #dddd44;}</style>";
+	}
+	//document.getElementById('text').innerHTML = light;
+	//showText(msg);
+	//return msg;
+	//$document.ready("Hello!");
+}
+
+function getCssProperty(elmId, property){
+   var elem = document.getElementById(elmId);
+   return window.getComputedStyle(elem,null).getPropertyValue(property);
+}
+// You could now get your value like
+//alert(getCssProperty("light", "background"));
+
+//Blur effect
+function setFocusQuery()
+{
+	$('#textInsideId').removeClass('blur-text');
+}
+function removeFocusQuery () {
+	$('#textInsideId').addClass('blur-text');
+}
---
+++ EventFeed/js/social.js
@@ -0,0 +1,8 @@
+function social() {
+	// body...
+	//document.write("<p>&copy;admixdev Inc. 2013</p>");
+	document.write("<button class='btn btn-twitter'><i class='icon-twitter'></i> <a href='http://www.twitter.com/admix_Snurnikov'>| Twitter</a></button>&nbsp&nbsp");
+    document.write("<button class='btn btn-linkedin'><i class='icon-linkedin'></i> <a href='http://ca.linkedin.com/in/alexandersnurnikov/''>| LinkedIn</a></button>&nbsp&nbsp");
+    document.write("<button class='btn btn-github'><i class='icon-github'></i> <a href='http://www.github.com/admix'>| Github</a></button>&nbsp&nbsp");
+    document.write("<button class='btn btn-instagram'><i class='icon-instagram'></i> <a href='http://www.admixdev.com'>| Blog</a></button>");
+}
